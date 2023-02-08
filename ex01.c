845f23d65fa53eed1f4b47899d787ca6754f6d4d
Sansus <santi02.fernandez.herrera@gmail.com>
2023-02-08 17:22:18 +0100

    main test for exercise 1

---
+++ ex01.c
@@ -0,0 +1,16 @@
+void	ft_putchar(char c);
+
+int		main(void){
+	ft_putchar('H');
+	ft_putchar('e');
+	ft_putchar('l');
+	ft_putchar(108);
+	ft_putchar('o');
+	ft_putchar(' ');
+	ft_putchar('W');
+	ft_putchar(111);
+	ft_putchar('r');
+	ft_putchar(108);
+	ft_putchar('d');
+	ft_putchar('\n');
+}
