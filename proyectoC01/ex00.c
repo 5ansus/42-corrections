395382f8da8d71019716f188cf6f079cb4168906
Sansus <santi02.fernandez.herrera@gmail.com>
2023-02-15 16:01:46 +0100

    Main for ex00 and ex01

---
+++ proyectoC01/ex00.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex00.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanferna <sanferna@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 15:50:51 by sanferna          #+#    #+#             */
+/*   Updated: 2023/02/15 15:56:09 by sanferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+void	ft_ft(int *nbr);
+
+int	main(void)
+{
+	int	x = -25;
+	int	y = 1000;
+	int	*puntero_x = &x;
+
+	printf("PRE FUNCION\n");
+	printf("Valor de x: %d -- Valor de y: %d", x, y);
+	ft_ft(puntero_x);
+	ft_ft(&y);
+	printf("POST FUNCION\n");
+	printf("Valor de x: %d -- Valor de y: %d", x, y);
+
+	return 0;
+}
---
+++ proyectoC01/ex01.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex00.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanferna <sanferna@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 15:50:51 by sanferna          #+#    #+#             */
+/*   Updated: 2023/02/15 15:56:09 by sanferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+void	ft_ultimate_ft(int *********nbr);
+
+int	main(void)
+{
+	int	x = 0;
+	int *puntero1 = &x;
+	int **puntero2 = &puntero1;
+	int ***puntero3 = &puntero2;
+	int ****puntero4 = &puntero3;
+	int *****puntero5 = &puntero4;
+	int ******puntero6 = &puntero5;
+	int *******puntero7 = &puntero6;
+	int ********puntero8 = &puntero7;
+	int *********puntero9 = &puntero8;
+
+	printf("PRE FUNCION\n");
+	printf("Valor de x: %d -- Valor de y: %d", x);
+	ft_ultimate_ft(puntero9);
+	printf("POST FUNCION\n");
+	printf("Valor de x: %d -- Valor de y: %d", x);
+
+	return 0;
+}
