0f78559d6d201f2aaa2a77ebb79e23ea0c478ecd
Santiago Fernandez Herrera <sanferna@c2r18s1.42madrid.com>
2023-02-16 12:43:36 +0100

    Hotfix C01

--- proyectoC01/ex07.c
+++ proyectoC01/ex07.c
@@ -6,7 +6,7 @@
 /*   By: sanferna <sanferna@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 15:50:51 by sanferna          #+#    #+#             */
-/*   Updated: 2023/02/15 15:56:09 by sanferna         ###   ########.fr       */
+/*   Updated: 2023/02/16 12:31:59 by sanferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,8 @@ int	main(void)
 		else
 			printf(", ");
 	}
-	ft_rev_int_tab(&x, 6);
-	ft_rev_int_tab(&y, 5);
+	ft_rev_int_tab(x, 6);
+	ft_rev_int_tab(y, 5);
 	printf("POST FUNCION - Reverse\n");
 	printf("x: ");
 	for(i = 0; i < 6; i++)
